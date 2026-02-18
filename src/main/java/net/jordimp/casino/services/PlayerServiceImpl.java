29316e23c317b3bf805f3af200dbbc1bb7c28590
jordi <jordi.marsal@gmail.com>
2026-02-18 10:50:34 +0100

    refactor: remove misleading @Transactional annotations from in-memory storage

--- src/main/java/net/jordimp/casino/services/PlayerServiceImpl.java
+++ src/main/java/net/jordimp/casino/services/PlayerServiceImpl.java
@@ -2,7 +2,6 @@ package net.jordimp.casino.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import net.jordimp.casino.dao.PlayerDAO;
 import net.jordimp.casino.entity.Player;
@@ -17,38 +16,31 @@ public class PlayerServiceImpl implements PlayerService {
 	}
 
 	@Override
-	@Transactional(readOnly = true)
 	public Player findByUUID(String uuid) {
 		return playerDAO.findByUUID(uuid);
 	}
 
 	@Override
-	@Transactional
 	public void delete(Player player) {
 		playerDAO.delete(player);
 	}
 
 	@Override
-	@Transactional
 	public void save(Player player) {
 		playerDAO.save(player);
 	}
 
 	@Override
-	@Transactional
 	public Player login(Player player) {
 		return playerDAO.login(player);
 	}
 
 	@Override
-	@Transactional
 	public boolean logout(String uuid) {
 		return playerDAO.logout(uuid);
 	}
 
 	public void purgeLogins() {
 		playerDAO.purgeLogins();
-		
 	}
-
 }
